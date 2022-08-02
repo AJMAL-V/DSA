c8bb7d918f5cacab5c43b9b8b25ff151fb7b4cf7
AJMAL-V <ajmalv86908@gmail.com>
2022-08-02 20:49:16 +0530

    array chnges

---
+++ LinkedList/singleLinkedList.java
@@ -0,0 +1,62 @@
+public class singleLinkedList {
+    class Node{
+        int data;
+        Node next;
+         Node (int data){
+             this.data=data;
+         }
+
+    }
+    public  Node head=null;
+    public  Node tail=null;
+     public void  addNode(int data){
+         Node newNode= new Node(data);
+         if (head==null){
+             head=newNode;
+         }else {
+             tail.next=newNode;
+         }
+         tail=newNode;
+     }
+     public  void  delete(int data){
+         Node temp=head,prev=null;
+         if (temp!=null && temp.data==data){
+             head=temp.next;
+             return;
+         }
+         while (temp!=null && temp.data!=data){
+             prev=temp;
+             temp=temp.next;
+         }
+         if (temp==null){
+             return;
+         }
+         if (temp==tail){
+             tail=prev;
+             tail.next=null;
+             return;
+         }
+         prev.next=temp.next;
+     }
+     public void  display(){
+         if (head==null){
+            System.out.println("empty");
+            return;
+         }
+         Node temp=head;
+         while (temp!=null){
+             System.out.println(temp.data);
+             temp=temp.next;
+         }
+
+     }
+     public  static void main(String arg[]){
+         singleLinkedList List=new singleLinkedList();
+         List.display();
+         List.addNode(10);
+         List.addNode(20);
+         List.display();
+         List.delete(20);
+         List.display();
+     }
+}
